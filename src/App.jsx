24afe95fc05fefa721de7fc94f7f1375273aaf5e
tarungautam137 <tarungautam137@gmail.com>
2025-03-18 19:16:25 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Body from './Body'
+import Login from './Login'
+import Profile from './Profile'
+import { BrowserRouter,Routes,Route } from 'react-router'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />} >
+            <Route path="/login" element={<Login/>} />
+            <Route path="/profile" element={<Profile/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
