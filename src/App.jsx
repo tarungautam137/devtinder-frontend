e23cf4dbeecc7a7ff99abe3c6a0e232c104066d2
tarungautam137 <tarungautam137@gmail.com>
2025-03-23 18:05:15 +0530

    cors and redux for login

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
-import Body from './Body'
-import Login from './Login'
-import Profile from './Profile'
+import Body from './components/Body'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import Feed from './components/feed'
 import { BrowserRouter,Routes,Route } from 'react-router'
+import { Provider } from 'react-redux'
+import appStore from "./utils/appStore"
 
 const App = () => {
   return (
     <>
+      <Provider store={appStore}>
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Body />} >
+            <Route path="/" element={<Feed />} />
             <Route path="/login" element={<Login/>} />
             <Route path="/profile" element={<Profile/>} />
           </Route>
         </Routes>
       </BrowserRouter>
+      </Provider>
     </>
   )
 }
