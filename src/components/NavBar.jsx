05ffaf6dcc008b990c4d5a3df4292f319fa2d775
tarungautam137 <tarungautam137@gmail.com>
2025-03-30 14:58:12 +0530

    if user is not logged in redirect to the login page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router'
 const NavBar = () => {
 
   const user=useSelector(store=>store.user)
@@ -7,7 +8,7 @@ const NavBar = () => {
     <div className="navbar bg-base-100 shadow-sm">
 
     <div className="flex-1">
-      <a className="btn btn-ghost text-xl">DevTinder</a>
+      <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
     </div>
 
     <div className="flex gap-4 mx-4">
@@ -25,9 +26,9 @@ const NavBar = () => {
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li>
-          <a className="justify-between">
+          <Link to="/profile" className="justify-between">
             Profile
-          </a>
+          </Link>
         </li>
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
